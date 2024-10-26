fb7af10f52aeede55856af09740ff79401aa4268
Egorpuzik <egor.egorov0004@gmail.com>
2024-10-26 18:57:12 +0200

    refactor: move common game logic to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-const runGame = () => {
-  // логика игры
+import readlineSync from 'readline-sync';
+
+const maxRounds = 3;
+
+const runGame = (description, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(description);
+
+  for (let round = 0; round < maxRounds; round += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default runGame;
+
